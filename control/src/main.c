4f2be74c0eac71671951ef8eeb96e918513c07c4
Jonathan Thomson <1591671+jonmthomson@users.noreply.github.com>
2026-03-05 11:16:58 +1300

    Fix shell via USB CDC

--- control/src/main.c
+++ control/src/main.c
@@ -1,10 +1,5 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/drivers/usb/usb_dc.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/device.h>
-#include <zephyr/usb/usb_device.h>
-#include <zephyr/usb/usbd.h>
 
 LOG_MODULE_REGISTER(control_app, LOG_LEVEL_INF);
 
